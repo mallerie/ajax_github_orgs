eb7ea911ea56238f2454c74cef52c41749065cb8
Mallerie Shirley <mallerie@gmail.com>
2016-03-15 13:25:26 -0400

    Another commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 // Javascript Entry Point
+
+import $ from 'jquery';
+
+
+var request = $.ajax({
+  url: 'https://api.github.com/users/wycats/orgs',
+  dataType: 'json',
+  method: 'get'
+});
+
+request.then(objects.forEach({login, avatar_url}) {
+  console.log(data);
+  `<div><img src="${data.avatar_url}"><p>${data.login}</p></div>`
+});
+
