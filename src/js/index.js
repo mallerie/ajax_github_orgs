0cb0d8f04649e6da3b614c7d212a2a731ccbeba5
Mallerie Shirley <mallerie@gmail.com>
2016-03-15 14:57:36 -0400

    Final Commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,30 @@
 
 import $ from 'jquery';
 
-
 var request = $.ajax({
   url: 'https://api.github.com/users/wycats/orgs',
   dataType: 'json',
   method: 'get'
+
 });
 
-request.then(objects.forEach({login, avatar_url}) {
-  console.log(data);
-  `<div><img src="${data.avatar_url}"><p>${data.login}</p></div>`
+
+request.then(function(response) {
+
+  response.forEach(function(data) {
+
+    var html = `<div><img src="${data.avatar_url}"><p>${data.login}</p></div>`
+
+    $('.app').append( html );
+
+  })
+
 });
+
+//This is how to put both functions on one line.
+// request.then(response => response.forEach(data => {
+//    var html = `<div><img src="${data.avatar_url}"><p>${data.login}</p></div>`
+
+//     $('.app').append( html );
+// }))
 
